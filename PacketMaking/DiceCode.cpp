5aa9ef7108f30f21bedc213811e10c40533cfcaa
T-Okada55 <co.muska@gmail.com>
2013-07-21 01:16:13 +0900

    (仮)全体の動作確認用 エンコード/デコード モジュール
    こんなエンコードでは勝てるわけがない
    あくまで動作の確認用

---
+++ PacketMaking/DiceCode.cpp
@@ -0,0 +1,58 @@
+﻿using namespace System;
+using namespace System::Text;
+using namespace System::Collections::Generic;
+
+namespace DiceCode
+{
+	//エンコード用
+	List<int>^ Encode(String^ text)
+	{
+		List<int>^ result = gcnew List<int>();
+		array<wchar_t>^ chars = text->ToCharArray();
+		for each (wchar_t var in chars)
+		{
+
+			//ここのアルゴリズムを改良する。
+			result->Add(var/25);
+			result->Add((var/5)%5);
+			result->Add(var%5);
+
+		}
+		return result;
+	}
+
+	//デコード用
+	String^ Decode(List<int>^ numbers)
+	{
+		StringBuilder^ sb = gcnew StringBuilder();
+		for (int i = 0; i < numbers->Count; i+=3)
+		{
+
+			//ここのアルゴリズムをエンコードによって変える
+			sb->Append((wchar_t)(numbers[i]*25+numbers[i+1]*5+numbers[i+2]));
+
+		}
+		return sb->ToString();
+	}
+}
+
+//#define Test
+#ifdef Test
+
+using namespace DiceCode;
+int main()
+{
+	Console::WriteLine("#teststart#");
+	String^ testMessage ="'a-!_iidesune\"";
+	Console::WriteLine("SendMessage:"+testMessage);
+	List<int>^ aaa=Encode(testMessage);
+	for each (int var in aaa)
+	{
+		Console::WriteLine(var);
+	}
+	Console::WriteLine("ReceiveMessage:"+Decode(aaa));
+	Console::WriteLine("#testend#");
+	return 0;
+}
+
+#endif
